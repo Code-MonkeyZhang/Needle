acc79c51d3079b7c4883904a52affc88a7d4e834
Code-MonkeyZhang <yufengzhang483@gmail.com>
2024-09-20 11:32:46 -0400

    add pybind 11

---
+++ build/CMakeFiles/ndarray_backend_cuda.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../python/needle/backend/backend_ndarray/ndarray_backend_cuda.cpython-311-x86_64-linux-gnu.so"
+  "../python/needle/backend/backend_ndarray/ndarray_backend_cuda.pdb"
+  "CMakeFiles/ndarray_backend_cuda.dir/src/ndarray_backend_cuda_generated_ndarray_backend_cuda.cu.o"
+  "CMakeFiles/ndarray_backend_cuda.dir/src/utilities/memory_pool.cpp.o"
+  "CMakeFiles/ndarray_backend_cuda.dir/src/utilities/memory_pool.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ndarray_backend_cuda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
